025f8dd2ecbb92999c16cceaa0fcd3b28640deb0
Gabriel Richardson <gabriel.richardson.97@gmail.com>
2019-01-17 12:26:07 -0600

    Find new jobs and add details to message

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ from time import sleep
 import os
 from twilio.rest import Client
 import config as c
+import pickle
 
 def login():
     browser = webdriver.Chrome(c.driver)
@@ -17,38 +18,69 @@ def login():
     submit = browser.find_element_by_id("qa-button-login")
     submit.click()
     
-    time.sleep(8)
+    time.sleep(10)
     return browser
 
 def get_jobs(browser):
     jobs = browser.find_element_by_id("availableJobs").find_elements_by_class_name("job")
+    new_jobs = {}
     for job in jobs:
-        print(job.find_element_by_class_name("name").text)
-        print(job.find_element_by_class_name("title").text)
-        print(job.find_element_by_class_name("itemDate").text)
-        print(job.find_element_by_class_name("startTime").text)
-        print(job.find_element_by_class_name("endTime").text)
-        print(job.find_element_by_class_name("locationName").text)
+        new_jobs[job.get_attribute("id")] = job
+    return new_jobs
 
-# def compare_jobs():
+def compare_jobs(new_jobs):
+    file = open("jobs_dict.txt", "rb")
+    old_jobs = pickle.load(file)
 
-# def update_jobs():
+    for job in old_jobs:
+        print(job)
+        if job not in new_jobs:
+            old_jobs.pop(job)
+    for job in new_jobs:
+        print("\n--\n")
+        print(job)
+        if job in old_jobs:
+            new_jobs.pop(job)
+        else:
+            old_jobs.update({job:new_jobs[job]})
+    
+    file = open("jobs_dict.txt", "wb")
+    pickle.dump(old_jobs, file)
+    file.close()
+    
+    return new_jobs
 
-def send_sms():
+def send_sms(jobs):
     account_sid = c.sid
     auth_token = c.auth
 
     client = Client(account_sid, auth_token)
 
+    for job in jobs:
+        details = job_details(jobs[job])
+        msg = "You have " + len(jobs) + " new job(s) available:\n" + details
+
     client.messages.create(
         to = c.me,
         from_ = c.twilio,
-        body = "Hello, world!"
+        body = msg
     )
+
+def job_details(job):
+    name = job.find_element_by_class_name("name").text
+    title = job.find_element_by_class_name("title").text
+    date = job.find_element_by_class_name("itemDate").text
+    start = job.find_element_by_class_name("startTime").text
+    end = job.find_element_by_class_name("endTime").text
+    location = job.find_element_by_class_name("locationName").text
+    return name + "\n" + title + "\nFrom " + start + " to " + end + " on " + date + " at " + location
 
 def main():
     browser = login()
-    get_jobs(browser)
+    new_jobs = get_jobs(browser)
+    new_jobs = compare_jobs(new_jobs)
+    if len(new_jobs) != 0:
+      send_sms(new_jobs)
 
 if __name__ == "__main__":
     main()
