40ae34b42e8f966820d3fb232e37e994c48ae02b
Gabriel Richardson <gabriel.richardson.97@gmail.com>
2019-01-17 01:38:39 -0600

    Get all job properties

--- scraper.py
+++ scraper.py
@@ -20,19 +20,15 @@ def login():
     time.sleep(8)
     return browser
 
-
-'''
-job id
-name
-title
-date
-location
-startTime
-endTime
-'''
 def get_jobs(browser):
     jobs = browser.find_element_by_id("availableJobs").find_elements_by_class_name("job")
-    print(jobs)
+    for job in jobs:
+        print(job.find_element_by_class_name("name").text)
+        print(job.find_element_by_class_name("title").text)
+        print(job.find_element_by_class_name("itemDate").text)
+        print(job.find_element_by_class_name("startTime").text)
+        print(job.find_element_by_class_name("endTime").text)
+        print(job.find_element_by_class_name("locationName").text)
 
 # def compare_jobs():
 
