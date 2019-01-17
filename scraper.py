0e1b3e5d73b09b80569a0e85484223ec29eac682
Gabriel Richardson <gabriel.richardson.97@gmail.com>
2019-01-16 23:06:23 -0600

    Ditch nodejs request module for selenium approach

---
+++ scraper.py
@@ -0,0 +1,17 @@
+import selenium
+from selenium import webdriver
+import time
+from time import sleep
+import config as c
+
+def login():
+    browser = webdriver.Chrome(c.driver)
+    browser.get(c.webpage)
+    time.sleep(2)
+
+    username = browser.find_element_by_id('Username')
+    username.send_keys(c.username)
+    password = browser.find_element_by_id('Password')
+    password.send_keys(c.password)
+    submit = browser.find_element_by_id('qa-button-login')
+    submit.click()
