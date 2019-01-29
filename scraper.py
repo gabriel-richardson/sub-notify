ee9c155642a64f5f0c567f15612d7d9e2e7bf2e6
Gabriel Richardson <gabriel.richardson.97@gmail.com>
2019-01-28 21:50:12 -0600

    Refresh before trying to log in on TimetoutException

--- scraper.py
+++ scraper.py
@@ -42,6 +42,7 @@ def get_jobs(browser):
             break
         except TimeoutException:
             print("Couldn't find job table, refreshing")
+            browser.refresh()
             login()
             browser.refresh()
 
